5c68dc80545dabe94365b2f16f3ec48719055ffd
mert.alptekin <mert.alptekin@neomina.com>
2023-03-21 14:45:19 +0300

    MultiLanguages

--- projects/app2/src/app/app.component.ts
+++ projects/app2/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TranslatePipe],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app2';
+  langs = ['', 'tr', 'en'];
+
+  constructor(private tService: TranslateService, private tp: TranslatePipe) {
+    this.tService.addLangs(this.langs);
+    // desteklenen dilleri tanıttık
+  }
+
+  ngOnInit(): void {
+    this.tService.onLangChange.subscribe((cLang: any) => {
+      // language değişiminde event tetiklenir.
+      console.log('cLang', cLang);
+    });
+  }
+
+  onLangChange(lng: string) {
+    console.log('lng', lng);
+    // translate service ile dil değişikliği yaptık
+
+    this.tService.use(lng); // dil değişimi yaptık
+
+    const value = this.tp.transform('hello');
+    const val2 = this.tService.instant('validations.min', {
+      min: 15,
+      value: 10,
+      max: 20,
+    });
+    console.log('translatePipe', value, val2);
+  }
 }
--- projects/app2/src/app/app.module.ts
+++ projects/app2/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en', // sayfalar varsıyalan olarak hangi dil üzerinde çalışsın
+      loader: {
+        provide: TranslateLoader, // ngxTranslate içerisinde TranslateLoader tipinde bir provider üzerinden işlem yap.
+        useFactory: createTranslateLoader, // createTranslateLoader bir sınıf üzerinden dil dosyalarını
+        deps: [HttpClient], // bu işlemin çalışması DI olarak service gönderilen deps tanımı
+      },
+    }), // lib için forRoot yazmaya gerek yok
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
